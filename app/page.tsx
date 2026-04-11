fe4501e0fb517fed688edd9001f82730ee4fc429
Kaym Kassai <kassai.kaym@gmail.com>
2026-04-11 07:12:12 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import HeroSection from '@/components/home/HeroSection';
-import AboutSection from '@/components/home/AboutSection';
-import ServicesSection from '@/components/home/ServicesSection';
-import CertificationsSection from '@/components/home/CertificationsSection';
-import WhyUsSection from '@/components/home/WhyUsSection';
-import TestimonialsSection from '@/components/home/TestimonialsSection';
-import ContactSection from '@/components/home/ContactSection';
+import HeroSection from "@/components/home/HeroSection";
+import AboutSection from "@/components/home/AboutSection";
+import ServicesSection from "@/components/home/ServicesSection";
+import CertificationsSection from "@/components/home/CertificationsSection";
+import WhyUsSection from "@/components/home/WhyUsSection";
+import TestimonialsSection from "@/components/home/TestimonialsSection";
+import ContactSection from "@/components/home/ContactSection";
 
 export default function Home() {
   return (
@@ -14,7 +14,6 @@ export default function Home() {
       <ServicesSection />
       <CertificationsSection />
       <WhyUsSection />
-      <TestimonialsSection />
       <ContactSection />
     </>
   );
